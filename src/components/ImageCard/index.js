050166d9fd349ee5bd6f81e687454132616692f3
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-01-24 22:35:00 -0500

    components added

---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default FriendCard;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+function Nav(){
+    return(
+        <nav>
+
+        </nav>
+    )
+}
+export default Nav;
