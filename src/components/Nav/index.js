4dfe3ef58db835609939f2ad15eea6e067ddf29c
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-01-26 00:07:44 -0500

    CSS modification

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function Nav(){
     return(
         <nav className ="navbar navbar-expand-lg navbar-dark">
-            <span className ="mb-0 h1"><h1 id="nav-text"> <strong> ClickyGame</strong></h1></span>
+            <span className ="mb-0 h1"><h1 id="nav-text"> <strong> </strong></h1></span>
 
         </nav>
 
